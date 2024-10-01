b0f302eed7a9ba156bcbf9ec9325cc52f1bb45aa
Pablo F. Guerra <pabloimrik17@gmail.com>
2024-10-01 08:58:09 +0200

    Merge pull request feature/add-models (#5) from feature/add-models
    * added event model
    
    * added reservation model
    
    * added user model

--- packages/domain-layer/src/index.ts
+++ packages/domain-layer/src/index.ts
@@ -1,3 +1,8 @@
 import "reflect-metadata";
 
+// Models
+export * from "./models/event.model.js";
+export * from "./models/reservation.model.js";
+
+// Infra
 export * from "./infra/http/http-client.js";
---
+++ packages/domain-layer/src/models/user.model.ts
@@ -0,0 +1,35 @@
+interface UserProps {
+    id: string;
+    name: string;
+    email: string;
+    passwordHash: string;
+}
+
+class User {
+    get id(): string {
+        return this.props.id;
+    }
+
+    get name(): string {
+        return this.props.name;
+    }
+
+    get email(): string {
+        return this.props.email;
+    }
+
+    get passwordHash(): string {
+        return this.props.passwordHash;
+    }
+
+    constructor(private readonly props: UserProps) {}
+
+    verifyPassword(password: string): boolean {
+        return this.hash(password) === this.passwordHash;
+    }
+
+    private hash(password: string): string {
+        // Simple hash implementation (for example purposes)
+        return password.split("").reverse().join("");
+    }
+}
