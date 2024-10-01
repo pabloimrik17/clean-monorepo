7194c4a0adc9604c2430cb366618fe987f243fa5
Pablo. Fernandez Guerra <pablo.fernandez@nazaries.com>
2024-10-01 15:59:12 +0200

    fixed circular ref and  add usecase

--- packages/domain-layer/src/usecases/reservation-create.usecase.ts
+++ packages/domain-layer/src/usecases/reservation-create.usecase.ts
@@ -1,9 +1,43 @@
 import { inject, injectable } from "inversify";
-import { type ReservationRepository } from "../repositories/reservation.repository.js";
-import { domainTypes } from "@repo/di";
+import type{  ReservationRepository } from "../repositories/reservation.repository.js";
+import type { EventRepository } from "../repositories/event.repository.js";
+import { Reservation } from "../models/reservation.model.js";
+import { DateTime } from "luxon";
+import { v4 as uuidv4 } from 'uuid';
+
+class EventNotActiveException extends Error {
+    constructor() {
+        super("The event is not active");
+    }
+}
+export class UnauthorizedAccessException extends Error {}
 
 @injectable()
 export class ReservationCreateUseCase {
-    constructor(@inject(domainTypes.reservationRepository) private readonly reservationRepository: ReservationRepository) {
+    constructor(
+        @inject("EventRepository") private readonly eventRepository: EventRepository,
+        @inject("ReservationRepository") private readonly reservationRepository: ReservationRepository
+    ) {}
+
+    async execute(userId: string, eventId: string): Promise<Reservation> {
+        const event = await this.eventRepository.getById(eventId);
+
+        if (!event) {
+            throw new EventNotActiveException();
+        }
+
+        const updatedEvent = event.decreaseCapacity();
+
+        const newReservation = new Reservation({
+            id: uuidv4(),
+            userId,
+            eventId,
+            reservationDate: DateTime.now(),
+            state: "active",
+        });
+
+        await this.reservationRepository.save(newReservation);
+
+        return newReservation;
     }
 }
