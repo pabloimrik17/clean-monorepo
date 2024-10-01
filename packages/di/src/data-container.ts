e244759441bcf509b18513be8e0561603f6037a1
Pablo. Fernandez Guerra <pablo.fernandez@nazaries.com>
2024-10-01 17:43:52 +0200

    fix di

--- packages/di/src/data-container.ts
+++ packages/di/src/data-container.ts
@@ -2,26 +2,9 @@ import { ConsoleLogHttpClient } from "@repo/data-layer";
 import { HttpClient } from "@repo/domain-layer";
 import { Container } from "inversify";
 
-export const dataTypes = {
-    infra: {
-        HttpClient: Symbol.for("HttpClient"),
-    },
-    repositories: {
-        EventRepository: Symbol.for("EventRepository"),
-        ReservationRepository: Symbol.for("ReservationRepository"),
-        UserRepository: Symbol.for("UserRepository"),
-    },
-};
-
 export const dataContainer = new Container();
 
-dataContainer
-    .bind<HttpClient>(dataTypes.infra.HttpClient)
-    .to(ConsoleLogHttpClient);
-dataContainer
-    .bind(dataTypes.repositories.EventRepository)
-    .toConstantValue(null);
-dataContainer
-    .bind(dataTypes.repositories.ReservationRepository)
-    .toConstantValue(null);
-dataContainer.bind(dataTypes.repositories.UserRepository).toConstantValue(null);
+dataContainer.bind<HttpClient>("HttpClient").to(ConsoleLogHttpClient);
+dataContainer.bind("EventRepository").toConstantValue(null);
+dataContainer.bind("ReservationRepository").toConstantValue(null);
+dataContainer.bind("UserRepository").toConstantValue(null);
--- packages/di/src/domain-container.ts
+++ packages/di/src/domain-container.ts
@@ -7,43 +7,17 @@ import {
 } from "@repo/domain-layer";
 import { Container } from "inversify";
 
-export const domainTypes = {
-    usecases: {
-        EventGetByIdUseCase: Symbol.for("EventGetByIdUseCase"),
-        EventListAvailableUseCase: Symbol.for("EventListAvailableUseCase"),
-        ReservationCancelUseCase: Symbol.for("ReservationCancelUseCase"),
-        ReservationCreateUseCase: Symbol.for("ReservationCreateUseCase"),
-        ReservationEditUseCase: Symbol.for("ReservationEditUseCase"),
-        UserCreateUseCase: Symbol.for("UserCreateUseCase"),
-        UserGetReservationsUseCase: Symbol.for("UserGetReservationsUseCase"),
-    },
-};
-
 export const domainContainer = new Container();
 
-domainContainer
-    .bind(domainTypes.usecases.EventGetByIdUseCase)
-    .to(EventGetByIdUseCase);
+domainContainer.bind("EventGetByIdUseCase").to(EventGetByIdUseCase);
 
-domainContainer
-    .bind(domainTypes.usecases.EventListAvailableUseCase)
-    .to(EventListAvailableUseCase);
+domainContainer.bind("EventListAvailableUseCase").to(EventListAvailableUseCase);
 
-domainContainer
-    .bind(domainTypes.usecases.ReservationCancelUseCase)
-    .to(ReservationCancelUseCase);
+domainContainer.bind("ReservationCancelUseCase").to(ReservationCancelUseCase);
 
-domainContainer
-    .bind(domainTypes.usecases.ReservationCreateUseCase)
-    .to(ReservationCreateUseCase);
+domainContainer.bind("ReservationCreateUseCase").to(ReservationCreateUseCase);
 
-domainContainer
-    .bind(domainTypes.usecases.ReservationEditUseCase)
-    .to(ReservationEditUseCase);
+domainContainer.bind("ReservationEditUseCase").to(ReservationEditUseCase);
 
-domainContainer
-    .bind(domainTypes.usecases.UserCreateUseCase)
-    .toConstantValue(null);
-domainContainer
-    .bind(domainTypes.usecases.UserGetReservationsUseCase)
-    .toConstantValue(null);
+domainContainer.bind("UserCreateUseCase").toConstantValue(null);
+domainContainer.bind("UserGetReservationsUseCase").toConstantValue(null);
