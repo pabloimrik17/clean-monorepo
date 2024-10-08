7a07efffd30170388b9b1700be622632c33ae719
Pablo. Fernandez Guerra <pablo.fernandez@nazaries.com>
2024-10-08 21:47:58 +0200

    added controllers

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,11 +1,18 @@
 import { Module } from "@nestjs/common";
 import { container } from "@repo/di";
-import { EventGetByIdUseCase } from "@repo/domain-layer";
+import {
+    EventGetByIdUseCase,
+    EventListAvailableUseCase,
+} from "@repo/domain-layer";
 import { EventsController } from "./events/events.controller";
 
 @Module({
     controllers: [EventsController],
     providers: [
+        {
+            provide: EventListAvailableUseCase,
+            useValue: container.get("EventListAvailableUseCase"),
+        },
         {
             provide: EventGetByIdUseCase,
             useValue: container.get("EventGetByIdUseCase"),
--- apps/api/src/events/events/events.controller.ts
+++ apps/api/src/events/events/events.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
+import {
+    Event,
+    EventGetByIdUseCase,
+    EventListAvailableUseCase,
+} from "@repo/domain-layer";
 
-@Controller('events')
-export class EventsController {}
+@Controller("events")
+export class EventsController {
+    constructor(
+        private readonly eventGetByIdUseCase: EventGetByIdUseCase,
+        private readonly eventListAvailableUseCase: EventListAvailableUseCase,
+    ) {}
+
+    @Get("active/list")
+    async listActiveEvents(): Promise<Event[]> {
+        return this.eventListAvailableUseCase.execute();
+    }
+
+    @Get(":id")
+    async getById(@Param("id") id: string): Promise<Event> {
+        return this.eventGetByIdUseCase.execute(id);
+    }
+}
