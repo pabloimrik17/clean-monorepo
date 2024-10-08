fb7c72a1f16a078455c2a2e75156eb5577465b66
Pablo. Fernandez Guerra <pablo.fernandez@nazaries.com>
2024-10-08 21:25:29 +0200

    added database connection and tables

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
 import { EventsModule } from "./events/events.module";
 import { ReservationsModule } from "./reservations/reservations.module";
 import { UsersModule } from "./users/users.module";
 
 @Module({
     imports: [UsersModule, EventsModule, ReservationsModule],
-    controllers: [AppController],
+    controllers: [],
 })
 export class AppModule {}
