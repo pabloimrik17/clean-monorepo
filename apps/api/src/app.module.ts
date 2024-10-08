b1b26adc302e1aa3a4c92da6677d998961f4391e
Pablo. Fernandez Guerra <pablo.fernandez@nazaries.com>
2024-10-08 15:53:03 +0200

    added modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,12 @@ import { container } from "@repo/di";
 import { EventGetByIdUseCase } from "@repo/domain-layer";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-    imports: [],
+    imports: [UsersModule, EventsModule, ReservationsModule],
     controllers: [AppController],
     providers: [
         AppService,
